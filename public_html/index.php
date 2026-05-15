43b666b87a2b7e02b477c2cc56bf1bdbb5bdd41c
Azan Asim <148682737+Azan-Asim@users.noreply.github.com>
2026-05-15 11:06:14 +0500

    done

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,24 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+if (str_starts_with($_SERVER['REQUEST_URI'] ?? '', '/storage/')) {
+    $storagePath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $relativePath = ltrim(substr($storagePath, strlen('/storage/')), '/');
+
+    if (! str_contains($relativePath, '..')) {
+        $filePath = realpath(__DIR__ . '/../storage/app/public/' . $relativePath);
+        $storageRoot = realpath(__DIR__ . '/../storage/app/public');
+
+        if ($filePath && $storageRoot && str_starts_with($filePath, $storageRoot) && is_file($filePath)) {
+            header('Content-Type: ' . (mime_content_type($filePath) ?: 'application/octet-stream'));
+            header('Cache-Control: public, max-age=31536000');
+            header('Content-Length: ' . filesize($filePath));
+            readfile($filePath);
+            exit;
+        }
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
